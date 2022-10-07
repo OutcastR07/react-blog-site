c6b938e406def2a750ef4198817652ad756fdb93
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2022-10-07 20:16:28 +0600

    fetching and error handling

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogss')
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error('could not fetch the data for that resource');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                })
+        }, 1000);
+    }, []);
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title='All Blogs!'></BlogList>}
         </div>
     );
 }
